3f030b83be751996824c2e77c138ae7ebd9f993a
Md. Nizam Uddin Shamrat <pridesys.0027@gmail.com>
2022-08-25 03:03:42 +0600

    NUSLauncher widget created.

--- lib/main.dart
+++ lib/main.dart
@@ -5,11 +5,18 @@ import 'package:get/get.dart';
 import 'app/routes/app_pages.dart';
 
 void main() {
-  runApp(
-    GetMaterialApp(
+  runApp(const NUSLauncher());
+}
+
+class NUSLauncher extends StatelessWidget {
+  const NUSLauncher({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
       title: "Application",
       initialRoute: AppPages.INITIAL,
       getPages: AppPages.routes,
-    ),
-  );
+    );
+  }
 }
