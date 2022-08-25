ba2736cbcbc3fa261dc7a3064324613dbce6763f
Md. Nizam Uddin Shamrat <pridesys.0027@gmail.com>
2022-08-25 12:56:22 +0600

    all component moved to app directory

---
+++ lib/app/components/custom_search_view.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class CustomSearchView extends StatelessWidget {
+  const CustomSearchView({Key? key, this.textEditingController}) : super(key: key);
+
+  final TextEditingController? textEditingController;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField();
+  }
+}
